198b86ff8d31cf5063677014eff19b607af52f20
jasonchenzhou <zhouchen@inside-65-93-175.wireless.utoronto.ca>
2015-06-17 13:15:41 -0400

    1

---
+++ leet.js
@@ -0,0 +1,7 @@
+var code_num = 'code/1.txt';
+
+function leet1(){code_num = 'code/1.txt'; document.getElementById("show_part").data=code_num;}
+function leet2(){code_num = 'code/2.txt'; document.getElementById("show_part").data=code_num;}
+
+
+
