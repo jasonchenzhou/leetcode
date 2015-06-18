dd7c6025bc714582d38b9145d725117b3f5cf913
jasonchenzhou <zhouchen@ZhouChens-MacBook-Pro.local>
2015-06-17 23:05:01 -0400

    3

--- leet.js
+++ leet.js
@@ -2,6 +2,7 @@ var code_num = 'code/1.txt';
 
 function leet1(){code_num = 'code/1.txt'; document.getElementById("show_part").data=code_num;}
 function leet2(){code_num = 'code/2.txt'; document.getElementById("show_part").data=code_num;}
+function leet2(){code_num = 'code/3.txt'; document.getElementById("show_part").data=code_num;}
 
 
 
