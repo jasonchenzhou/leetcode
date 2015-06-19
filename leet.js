6e4e5863c99bf9f800c8416ae82a156176100186
jasonchenzhou <zhouchen@inside-65-93-175.wireless.utoronto.ca>
2015-06-19 13:06:01 -0400

    6

--- leet.js
+++ leet.js
@@ -3,6 +3,9 @@ var code_num = 'code/1.txt';
 function leet1(){code_num = 'code/1.txt'; document.getElementById("show_part").data=code_num;}
 function leet2(){code_num = 'code/2.txt'; document.getElementById("show_part").data=code_num;}
 function leet3(){code_num = 'code/3.txt'; document.getElementById("show_part").data=code_num;}
+function leet4(){code_num = 'code/4.txt'; document.getElementById("show_part").data=code_num;}
+function leet5(){code_num = 'code/5.txt'; document.getElementById("show_part").data=code_num;}
+function leet6(){code_num = 'code/6.txt'; document.getElementById("show_part").data=code_num;}
 
 
 
